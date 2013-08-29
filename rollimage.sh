c9644e9dff0f79567cc4a2221264f4502c8834f4
Patrick L Archibald <patrick.archibald@gmail.com>
2013-08-29 19:45:48 -0400

    Add rollimage.sh script

---
+++ rollimage.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+cd /mnt/disk0/tmp
+j=0
+rm _ubuntu*.png
+for i in {100..150}
+do
+        let j=j+35
+	convert -roll +$j+$j  ubuntu.png _ubuntu$i.png
+done
+convert -limit memory 2000 -loop 0 -resize 400 -dispose previous _ubuntu*.png ubuntu.gif
+xdg-open ubuntu.gif
