ab206f936fb19c762007a4b08eddcaee412082a5
Patrick L Archibald <patrick.archibald@gmail.com>
2014-05-31 13:51:03 -0400

    Add github URL to comments.

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 #
-# Install packages on a new Ubuntu installation. 
+# Install packages on a new Ubuntu installation.
+# https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
 sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl
