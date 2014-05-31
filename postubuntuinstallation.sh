d273a664b98fde2fae55269eeb8d4ffde63873f2
Patrick L Archibald <patrick.archibald@gmail.com>
2014-05-31 13:48:42 -0400

    Initial commit.

---
+++ postubuntuinstallation.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Install packages on a new Ubuntu installation. 
+#
+sudo apt-get update
+sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl
