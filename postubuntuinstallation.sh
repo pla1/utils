d573aee7cf761ca0b0f44168b52fd0a9871955a0
Patrick L Archibald <patrick.archibald@gmail.com>
2014-05-31 14:10:28 -0400

    Added ttf-mscorefonts-installer

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,4 +4,4 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc
+sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer
