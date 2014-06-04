da7336d73840167a84f95ff85136048201fa7f8c
Patrick L Archibald <patrick.archibald@gmail.com>
2014-06-04 10:42:10 -0400

    Add nmap.

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,4 +4,4 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop
+sudo apt-get install gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap
