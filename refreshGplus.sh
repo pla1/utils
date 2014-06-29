b514855681d141f419888c345237591713034db5
Patrick L Archibald (PLA) <Patrick.Archibald@gmail.com>
2014-06-29 06:58:20 -0400

    Use J to advance through 100 posts and then L to refresh the stream.

--- refreshGplus.sh
+++ refreshGplus.sh
@@ -1,10 +1,17 @@
-
 #!/bin/bash
+#
+# Refresh Google Plus stream via kiosk mode
+#
 while :
 do
 	win=$(xdotool search --onlyvisible "Google+" | head -1)
 	echo "Window: $win"
+	for i in `seq 1 100`;
+        do
+		xdotool type --window $win "j"
+		sleep 3s
+        done
 	xdotool type --window $win "l"
-	sleep 10s
+	sleep 3s
 done
 
