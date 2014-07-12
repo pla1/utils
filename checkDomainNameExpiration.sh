b11fd9e3be8392265aab6028264c870fc1c0394e
Patrick L Archibald <patrick.archibald@hometelco.com>
2014-07-12 18:15:05 -0400

    Check domain name expiration

---
+++ checkDomainNameExpiration.sh
@@ -0,0 +1,30 @@
+#!/bin/bash
+#
+# Send email when domain name expires in 30 days or less.
+#
+DOMAIN_NAME=$1
+EMAIL_ADDRESS=$2
+if [ -z "$DOMAIN_NAME" ]  || [ -z "$EMAIL_ADDRESS" ]
+then
+  echo "Please pass the domain name in the first parameter. Email address should be the second parameter."
+  exit -1
+fi
+LABEL="Registrar Registration Expiration Date: "
+dateString=$(whois "$DOMAIN_NAME" | grep "$LABEL" | cut -d':' -f 2)
+if [ -z "$dateString" ]
+then
+  message="WHOIS query did not return a date string for domain name $DOMAIN_NAME"
+  echo "$message"
+  echo "$message" | /usr/bin/mail -s "$message" "patrick.archibald@hometelco.com"
+  exit
+fi
+d1=$(date -d "$dateString" +%s)
+d2=$(date -d "now - 30 days" +%s)
+days=$(( (d1 - d2) / 86400 ))
+echo "Domain name $DOMAIN_NAME expires in $days days"
+if [[ $days -lt 30 ]]
+then
+  message="Domain name registration expires in $days for domain name $DOMAIN_NAME"
+  echo "$message"
+  echo $message | /usr/bin/mail -s "$message" "$EMAIL_ADDRESS"
+fi
