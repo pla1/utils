43fb9acd546559bb8495fe58e3e2667af60bd63a
Patrick L Archibald <patrick.archibald@gmail.com>
2014-08-04 09:57:30 -0400

    Added python-pip

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,4 +4,4 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install parcellite gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc
+sudo apt-get install parcellite gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip
