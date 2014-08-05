ab098f5cc6601d345deca0959a2e6c7e4b06b31e
Patrick Archibald <Patrick.Archibald@gmail.com>
2014-08-05 11:16:37 -0400

    Added blurb about requiring expect in the comment section.

--- speedtestLogger.sh
+++ speedtestLogger.sh
@@ -6,6 +6,8 @@
 #
 # speedtest-cli lives here: https://github.com/sivel/speedtest-cli
 #
+# Requires expect. sudo apt-get install expect
+#
 set outputFile [open /tmp/output.txt w]
 exp_internal 1
 spawn speedtest-cli
@@ -29,5 +31,5 @@ expect -re ".*Upload: (\[0-9\]+\.\[0-9\]+) (.*)\r\n.*" {
 	set uploadUnit $expect_out(2,string)
 }
 puts $outputFile "curl --verbose --include --header Content-Type:application/json --header token:6bbc6fe7-5473-4d04-9468-900755 --request PUT --data '{\"download\":$download,\"upload\":$upload}' https://e.hometelco.com/a/SpeedtestUpload"
-exec /bin/sh /tmp/output.txt
+exec /bin/bash /tmp/output.txt
 
