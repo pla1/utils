5b1ed0be6cefdcfad1b73f9c83f0a1257d1cad3e
Patrick Archibald <Patrick.Archibald@gmail.com>
2014-08-05 13:54:52 -0400

    Store token in /etc directory

--- speedtestLogger.sh
+++ speedtestLogger.sh
@@ -2,8 +2,18 @@
 #
 # Call Expect script and executes the output.
 #
+TOKEN=$(cat /etc/speedtestLogger-token)
+if [ -z "$TOKEN" ] 
+then
+	HOSTNAME=$(hostname -i)
+	message="Speedtest token missing. Speed test aborted. $HOSTNAME"
+	echo "$message"
+	logger "$message"
+	echo "$message" | mail -s "$message" root
+	exit
+fi
 OUTPUT_SCRIPT_FROM_EXPECT="/tmp/speedtestLoggerOutput.sh"
 rm --force "$OUTPUT_SCRIPT_FROM_EXPECT"
 RUNNING_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$RUNNING_DIRECTORY/speedtestLogger.exp
+$RUNNING_DIRECTORY/speedtestLogger.exp "$TOKEN"
 /bin/bash "$OUTPUT_SCRIPT_FROM_EXPECT"
