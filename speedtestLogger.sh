e9a8437b235ccc78597368fa4215b2114e2d012d
Patrick Archibald <Patrick.Archibald@gmail.com>
2014-08-05 12:56:16 -0400

    Use current running directory to call expect script.

--- speedtestLogger.sh
+++ speedtestLogger.sh
@@ -2,5 +2,8 @@
 #
 # Call Expect script and executes the output.
 #
-./speedtestLogger.exp
-/bin/bash /tmp/speedtestLoggerOutput.sh
+OUTPUT_SCRIPT_FROM_EXPECT="/tmp/speedtestLoggerOutput.sh"
+rm --force "$OUTPUT_SCRIPT_FROM_EXPECT"
+RUNNING_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+$RUNNING_DIRECTORY/speedtestLogger.exp
+/bin/bash "$OUTPUT_SCRIPT_FROM_EXPECT"
