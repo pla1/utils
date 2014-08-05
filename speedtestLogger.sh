402a2aebdda1f61a728554e4db513b5199318500
Patrick Archibald <Patrick.Archibald@gmail.com>
2014-08-05 14:48:27 -0400

    Added log function.

--- speedtestLogger.sh
+++ speedtestLogger.sh
@@ -2,21 +2,24 @@
 #
 # Call Expect script and executes the output.
 #
+function log() {
+	echo "$1"
+	logger "$1"
+}
 TOKEN=$(cat /etc/speedtestLogger-token)
 if [ -z "$TOKEN" ] 
 then
 	HOSTNAME=$(hostname)
 	IP=$(curl http://myip.pla1.net)
 	message="Speedtest token missing. Speed test aborted. $HOSTNAME $IP"
-	echo "$message"
-	logger "$message"
+	log "$message"
 	echo "$message" | mail -s "$message" root
 	exit
 fi
-logger "$0 started."
+log "$0 started."
 OUTPUT_SCRIPT_FROM_EXPECT="/tmp/speedtestLoggerOutput.sh"
 rm --force "$OUTPUT_SCRIPT_FROM_EXPECT"
 RUNNING_DIRECTORY="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
-$RUNNING_DIRECTORY/speedtestLogger.exp "$TOKEN"
+"$RUNNING_DIRECTORY"/speedtestLogger.exp "$TOKEN"
 /bin/bash "$OUTPUT_SCRIPT_FROM_EXPECT"
-logger "$0 ended."
+log "$0 ended."
