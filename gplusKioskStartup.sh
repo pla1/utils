940500bfb1098c0c78f01267bb39e09873af39de
Patrick L Archibald <patrick.archibald@gmail.com>
2014-10-11 19:03:44 -0400

    Google Plus kiosk boot startup script.

---
+++ gplusKioskStartup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+SECONDS=10
+logger "$0 Sleeping for $SECONDS seconds"
+sleep $SECONDS
+logger "$0 changing monitor to portrait mode."
+xrandr -o left
+sleep 1
+logger "$0 Start Gplus auto-refresh kiosk"
+#/home/htplainf/utils/osdTime.sh &
+/home/htplainf/utils/refreshGplus.sh &
+dimensions=$(xrandr | grep '*' | cut -d ' ' -f 4)
+width=$(echo $dimensions | cut -d 'x' -f 2)
+width=$(($width - 100))
+echo "Width: $width"
+cairo-clock --xposition $width --yposition 0 --ontop &
