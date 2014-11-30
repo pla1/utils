6dc6daf50fd8dcc89621ebe180d2d90d50a3d97c
Patrick L Archibald <patrick.archibald@gmail.com>
2014-11-30 11:32:50 -0500

    Increase initial sleep time.

--- gplusKioskStartup.sh
+++ gplusKioskStartup.sh
@@ -4,12 +4,13 @@ logger "$0 Sleeping for $SECONDS seconds"
 sleep $SECONDS
 logger "$0 changing monitor to portrait mode."
 xrandr -o left
-sleep 1
+sleep 3
 logger "$0 Start Gplus auto-refresh kiosk"
-#/home/htplainf/utils/osdTime.sh &
-/home/htplainf/utils/refreshGplus.sh &
+/home/htplainf/utils/osdTime.sh &
 dimensions=$(xrandr | grep '*' | cut -d ' ' -f 4)
 width=$(echo $dimensions | cut -d 'x' -f 2)
 width=$(($width - 100))
 echo "Width: $width"
-cairo-clock --xposition $width --yposition 0 --ontop &
+#cairo-clock --xposition $width --yposition 0 --ontop &
+/home/htplainf/utils/refreshGplus.sh &
+
