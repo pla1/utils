24f3639a4f801a07ec487f0e8a55cc0b50d62efc
Patrick L Archibald (PLA) <Patrick.Archibald@gmail.com>
2015-02-22 10:25:09 -0500

    Add -y to avoid prompt on apt-get install command.

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,4 +4,4 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass
+sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass -y
