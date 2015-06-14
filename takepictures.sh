6726e7ab812aee495b048042f69ca45cccf288c3
Patrick L Archibald <patrick.archibald@gmail.com>
2015-06-14 13:34:33 -0400

    Change folder for Nikon D40X.

--- takepictures.sh
+++ takepictures.sh
@@ -1,12 +1,15 @@
-#!/bin/bash 
+#!/bin/bash
 # Take and store photos using gphoto2
+folder=~/Pictures/$(date +"%Y/%m/%d")
+echo "Pictures will be stored in $folder"
+mkdir -p "$folder"
 while true :
 do
-	gphoto2 --capture-image --get-file capt0000.jpg --folder /store_00010001 --hook-script test-hook.sh --force-overwrite
-	NOW=$(date +"%Y-%m-%d-%H-%M-%S")
-	OUTFILE="image-$NOW.jpg"
-	echo $OUTFILE
-	cp capt0000.jpg $OUTFILE
-	sleep 10;
+	gphoto2 --capture-image --get-file capt0000.jpg --folder / --hook-script test-hook.sh --force-overwrite
+	now=$(date +"%Y-%m-%d-%H-%M-%S")
+	file="image-$now.jpg"
+	echo "Storing $folder/$file"
+	mv capt0000.jpg "$folder/$file"
+	sleep 60;
 done
 
