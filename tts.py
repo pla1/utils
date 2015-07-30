cc966badfef17388b330fec7cbe379922f12fc09
Patrick L Archibald <patrick.archibald@gmail.com>
2015-07-30 08:09:56 -0400

    Drop the file name from the parameter list. Create file name from md5 hash of the text. If the file exists use it instead of creating it again.

--- tts.py
+++ tts.py
@@ -8,11 +8,7 @@
 #  same => n,agi(tts.py,"This text will be translated to voice by Ivona.com.","demo.mp3","2")
 #  same => n,Hangup()
 #
-import sys
-import os
-import pyvona
-import datetime
-import logging
+import sys,os,pyvona,datetime,logging,hashlib
 from subprocess import call
 
 def agi_command(cmd):
@@ -29,7 +25,17 @@ while True:
   if not len(line):
     break
   logging.info(line)
-
+text=sys.argv[1]
+escapeDigits=sys.argv[2]
+md5=hashlib.md5()
+md5.update(text);
+fileName=md5.hexdigest()+".mp3"
+directory="/tmp/"
+sourceFile=directory+fileName
+destinationFile=directory+"new"+os.path.splitext(fileName)[0]+".wav"
+if os.path.exists(destinationFile):
+    agi_command("STREAM FILE "+os.path.splitext(destinationFile)[0]+" \""+escapeDigits+"\"")
+    sys.exit()
 with open("/etc/aws.properties") as f:
     keys = f.readlines()
 AWS_ACCESS_KEY = keys[0].strip('\n')
@@ -39,12 +45,7 @@ voice = pyvona.create_voice(AWS_ACCESS_KEY, AWS_SECRET_KEY)
 voice.voice_name = "Salli"
 voice.speech_rate = 'fast'
 voice.codec = 'mp3'
-text=sys.argv[1]
-fileName=sys.argv[2]
-escapeDigits=sys.argv[3]
-directory="/tmp/"
 voice.fetch_voice(text,directory+fileName)
-sourceFile=directory+fileName
-destinationFile=directory+"new"+os.path.splitext(fileName)[0]+".wav"
 return_code = call(["/usr/bin/avconv","-loglevel","quiet","-y","-i",sourceFile,"-ar","8000","-ac","1","-ab","64",destinationFile])
 agi_command("STREAM FILE "+os.path.splitext(destinationFile)[0]+" \""+escapeDigits+"\"")
+sys.exit()
