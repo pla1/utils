80d49c9ad4145eab3009549d2da52e841f471ec8
Patrick L Archibald <patrick.archibald@gmail.com>
2015-08-14 11:10:48 -0400

    Added comments.

--- screenshotprocessor.sh
+++ screenshotprocessor.sh
@@ -1,7 +1,11 @@
 #!/bin/bash
-#
+# Screenshot Processor
 # Called from incron when a file arrives in ~/Pictures.
 # Copies file to /tmp/ss and adds a drop shadow.
+# Directory /tmp/ss is used so that I don't have to weed through
+# dozens of files in ~/Pictures when I embed the screenshot in an email.
+# incrontab entry sample below:
+# /home/user_joe/Pictures IN_CLOSE_WRITE /home/user_joe/utils/screenshotprocessor.sh $@/$#
 #
 filePath="$1"
 file=$(basename "$filePath")
