4bf3d0a08029b153db3c67e077344ad52dbc8e76
Patrick L Archibald <patrick.archibald@gmail.com>
2015-10-07 10:21:33 -0400

    Added mpg123

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,4 +4,4 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass wakeonlan ttytter -y
+sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass wakeonlan ttytter mpg123 -y
