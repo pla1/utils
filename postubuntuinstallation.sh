c0458cffc1c9eee43586c662f1cae49dddd10b6a
Patrick L Archibald <patrick.archibald@gmail.com>
2015-10-15 08:24:22 -0400

    Remove apport.

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -5,3 +5,4 @@
 #
 sudo apt-get update
 sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass wakeonlan ttytter mpg123 -y
+sudo apt-get remove apport
