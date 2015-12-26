bb312d35ff3ea2d560d645a407045e903314d738
Patrick L Archibald <patrick.archibald@gmail.com>
2015-12-26 16:39:04 -0500

    Added Yubikey neo manager

--- postubuntuinstallation.sh
+++ postubuntuinstallation.sh
@@ -4,7 +4,7 @@
 # https://github.com/pla1/utils/blob/master/postubuntuinstallation.sh
 #
 sudo apt-get update
-sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass wakeonlan ttytter mpg123 -y
+sudo apt-get install clipit gimp hdhomerun-config openssh-server rdesktop git wireshark compizconfig-settings-manager pwgen ipcalc curl vpnc ttf-mscorefonts-installer guvcview git htop nmap iotop iperf screen whois mediainfo mplayer vlc python-pip fail2ban expect iptraf iperf at uptimed pass wakeonlan ttytter mpg123 yubikey-neo-manager -y
 sudo apt-get remove apport -y
 wget https://dl.google.com/linux/direct/google-chrome-stable_current_amd64.deb
 sudo dpkg -i google-chrome-stable_current_amd64.deb
