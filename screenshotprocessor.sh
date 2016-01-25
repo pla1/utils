4323865dc183fa2eab99b1a96588c0458749169a
Patrick L Archibald <Patrick.Archibald@hometelco.com>
2016-01-25 13:45:09 -0500

    Moved the Python script above the notify-send command. Added to file comment.

--- screenshotprocessor.sh
+++ screenshotprocessor.sh
@@ -4,6 +4,7 @@
 # Copies file to /tmp/ss and adds a drop shadow.
 # Directory /tmp/ss is used so that I don't have to weed through
 # dozens of files in ~/Pictures when I embed the screenshot in an email.
+# Also copies the image file to the clipboard.
 # incrontab entry sample below:
 # /home/user_joe/Pictures IN_CLOSE_WRITE /home/user_joe/utils/screenshotprocessor.sh $@/$#
 #
@@ -16,6 +17,6 @@ cp "$filePath" /tmp/ss/.
 newFilePath="/tmp/ss/$file"
 logger "$0 Processing $filePath to $newFilePath"
 convert "$newFilePath" \( -clone 0 -background black -shadow 80x3+0+8 \) -reverse -background none -layers merge +repage "$newFilePath"
-/usr/bin/notify-send --icon=/usr/share/icons/Humanity/apps/48/gnome-screenshot.svg "Screenshot ready" "File is $newFilePath"
 /usr/bin/python ~/utils/image2clipboard.py "$filePath"
+/usr/bin/notify-send --icon=/usr/share/icons/Humanity/apps/48/gnome-screenshot.svg "Screenshot ready" "File is $newFilePath"
 logger "$0 Screenshot ready $newFilePath"
