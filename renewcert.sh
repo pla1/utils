a210cd9cfb358326d5fd48d542c610f274ff9888
Patrick L Archibald <htplainf@pla1.lcl>
2016-01-26 15:15:09 -0500

    Added pla1.info.

--- renewcert.sh
+++ renewcert.sh
@@ -2,9 +2,16 @@
 #
 # Renew Let's Encrypt certificate.
 #
+if [ "$(id -u)" != "0" ]; then
+   echo "This script must be run as root" 1>&2
+   exit 1
+fi
+
 /usr/bin/curl -vs https://30passwords.com 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://pla1.info 2>&1 | grep 'expire date:'
 cd /home/htplainf/letsencrypt
-sudo -su htplainf git pull
-./letsencrypt-auto certonly --apache --renew-by-default -d 30passwords.com -d www.30passwords.com
+/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d 30passwords.com -d www.30passwords.com
+/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d pla1.net
 /usr/sbin/service apache2 reload
 /usr/bin/curl curl -vs https://30passwords.com 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://pla1.net 2>&1 | grep 'expire date:'
