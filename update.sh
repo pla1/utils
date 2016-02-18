e32dc81b08e6d6fc687c702e2b353db7b3d3011c
Patrick L Archibald (PLA) <patrick.archibald@gmail.com>
2016-02-17 20:11:07 -0500

    Correct command syntax.

--- update.sh
+++ update.sh
@@ -2,4 +2,4 @@
 #
 # Update Debian system
 #
-sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-autoremove -y
+sudo apt-get update && sudo apt-get dist-upgrade && sudo apt-get autoremove -y
