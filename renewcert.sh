8beca4ce1d0fd15413e26c2157fb03c2b7c6df4b
Patrick L Archibald <patrick.archibald@gmail.com>
2016-07-03 18:08:57 -0400

    Added additional domain names.

--- renewcert.sh
+++ renewcert.sh
@@ -9,9 +9,15 @@ fi
 
 /usr/bin/curl -vs https://30passwords.com 2>&1 | grep 'expire date:'
 /usr/bin/curl -vs https://pla1.info 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://ip.pla1.info 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://canyoureadmymind.com 2>&1 | grep 'expire date:'
 cd /home/htplainf/letsencrypt
 /home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d 30passwords.com -d www.30passwords.com
-/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d pla1.net
+/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d pla1.info
+/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d ip.pla1.net
+/home/htplainf/letsencrypt/letsencrypt-auto --verbose certonly --apache --renew-by-default -d canyoureadmymind.com
 /usr/sbin/service apache2 reload
 /usr/bin/curl curl -vs https://30passwords.com 2>&1 | grep 'expire date:'
 /usr/bin/curl -vs https://pla1.net 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://ip.pla1.net 2>&1 | grep 'expire date:'
+/usr/bin/curl -vs https://canyoureadmymind.com 2>&1 | grep 'expire date:'
