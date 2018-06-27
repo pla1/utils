653c40df1c165b19dc2be9cd3a4ab26bf08059e9
Patrick L Archibald <patrick.archibald@gmail.com>
2018-06-27 08:11:12 -0400

    Align windows on desktop at work.

---
+++ windowAlignmentWorkDesktop.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+#
+# Position windows on desktop.
+#
+/usr/bin/wmctrl -a 'troubles' -e '0,3260,1,600,415'
+/usr/bin/wmctrl -a 'Southeast Radar' -e '0,3260,475,600,560'
+/usr/bin/wmctrl -a 'Home Queue Entries' -e '0,1920,1080,1920,1080'
+/usr/bin/wmctrl -a 'Provision' -e '0,1920,1080,1920,1080'
+/usr/bin/wmctrl -a 'Map' -e '0,1920,1080,1920,1080'
+/usr/bin/wmctrl -a 'Status' -e '0,1,1,1045,950'
+/usr/bin/wmctrl -a 'Pluma' -e '0,1,1408,1000,750'
+/usr/bin/wmctrl -a '- Google Chrome' -e '0,965,540,1920,1080'
+/usr/bin/wmctrl -a '— Atom' -e '0,965,540,1920,1080'
+/usr/bin/wmctrl -a 'Eclipse' -e '0,965,540,1920,1080'
