b39e852721adb6baea2277bec597b7d68d08654e
Patrick L Archibald <patrick.archibald@gmail.com>
2018-06-27 08:23:08 -0400

    Move Google Chrome to last.

--- windowAlignmentWorkDesktop.sh
+++ windowAlignmentWorkDesktop.sh
@@ -9,6 +9,6 @@
 /usr/bin/wmctrl -a 'Map' -e '0,1920,1080,1920,1080'
 /usr/bin/wmctrl -a 'Status' -e '0,1,1,1045,950'
 /usr/bin/wmctrl -a 'Pluma' -e '0,1,1408,1000,750'
-/usr/bin/wmctrl -a '- Google Chrome' -e '0,965,540,1920,1080'
 /usr/bin/wmctrl -a '— Atom' -e '0,965,540,1920,1080'
 /usr/bin/wmctrl -a 'Eclipse' -e '0,965,540,1920,1080'
+/usr/bin/wmctrl -a '- Google Chrome' -e '0,965,540,1920,1080'
