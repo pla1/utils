d082061fd7b7a737900c1aeff1422a2675a618ac
Patrick L Archibald <patrick.archibald@gmail.com>
2018-06-28 11:05:32 -0400

    Sort member list because line order varies.

--- keybaseTeamMembersLogger.sh
+++ keybaseTeamMembersLogger.sh
@@ -16,7 +16,15 @@ temporaryFile="/tmp/keybase_members.txt"
 diffFile="/tmp/keybase_members_diff.txt"
 yyyymmdd=$(date +%Y%m%d)
 echo "Date: $yyyymmdd"
-/usr/bin/keybase team list-members "$team" > "$temporaryFile"
+newFile="/keybase/team/$team/$team"
+newFile+="_members_"
+newDiffFile="$newFile"
+newDiffFile+="diff_"
+newFile+="$yyyymmdd"
+newDiffFile+="$yyyymmdd"
+newDiffFile+=".txt"
+newFile+=".txt"
+/usr/bin/keybase team list-members "$team" | sort > "$temporaryFile"
 filter="/keybase/team/$team/$team"
 filter+="_members_*.txt"
 echo "Filter: $filter"
@@ -26,24 +34,24 @@ do
   [[ $file -nt $latestFile ]] && latestFile=$file
 done
 echo "Latest file: \"$latestFile\""
-echo "Comparing files $latestFile to $temporaryFile"
-/usr/bin/diff "$latestFile" "$temporaryFile" > "$diffFile"
-if [ -s "$diffFile" ]
+if [[ ! -f $latestFile ]]
 then
-  echo "No differences found between $latestFile and /tmp/keybase_members.txt"
+  echo "File not found. Will create initial file."
+  cp "$temporaryFile" "$newFile"
   exit 0
-else
+fi
+cp "$latestFile" "/tmp/."
+latestFileBaseName=$(basename "$latestFile")
+echo "Comparing files /tmp/$latestFileBaseName to $temporaryFile"
+/usr/bin/diff "/tmp/$latestFileBaseName" "$temporaryFile" > "$diffFile"
+if [[ -s $diffFile ]]
+then
+  echo -e "Begin contents of diff file $diffFile on following line(s)."
   cat "$diffFile"
-  newFile="/keybase/team/$team/$team"
-  newFile+="_members_"
-  newDiffFile="$newFile"
-  newDiffFile+="diff_"
-  newFile+="$yyyymmdd"
-  newDiffFile+="$yyyymmdd"
-  newDiffFile+=".txt"
-  newFile+=".txt"
+  echo -e "End contents of diff file $diffFile."
   cp "$diffFile" "$newDiffFile"
   cp "$temporaryFile" "$newFile"
-  echo "Differences found between $latestFile and /tmp/keybase_members.txt Created new diff file: $newDiffFile"
-  exit 0
+  echo "Differences found between $latestFile and $temporaryFile Created new diff file: $newDiffFile"
+else
+  echo "No differences found between $latestFile and $temporaryFile"
 fi
