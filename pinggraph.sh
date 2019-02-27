9e995329e31e87f77077816f42ff044a12a0278d
Patrick L Archibald <patrick.archibald@gmail.com>
2019-02-27 11:51:07 -0500

    Enable multiple IP addresses

--- pinggraph.sh
+++ pinggraph.sh
@@ -1,3 +1,20 @@
 #!/bin/bash
-echo "Ping $1"
-ping -D "$1"
+#
+# PING one or more IP addresses for websocketd to consume for displaying a chart via pinggraph.html
+#
+set -e
+killChildredOnInterrupt () {
+  echo "Going to kill children"
+  ps --ppid $$
+  kill $(ps -o pid= --ppid $$)
+  exit 0
+}
+trap killChildredOnInterrupt EXIT
+for ip in "$@"
+do
+  ping -n -D "$ip" &
+done
+while true
+do
+  sleep 1
+done
