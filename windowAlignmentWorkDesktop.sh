d89bed27afa8169353990222e42ee064460ba310
Patrick L Archibald <patrick.archibald@gmail.com>
2019-06-06 15:01:16 -0400

    Increase width of Status window.

--- windowAlignmentWorkDesktop.sh
+++ windowAlignmentWorkDesktop.sh
@@ -7,7 +7,7 @@
 /usr/bin/wmctrl -a 'Home Queue Entries' -e '0,1920,1080,1920,1080'
 /usr/bin/wmctrl -a 'Provision' -e '0,1920,1080,1920,1080'
 /usr/bin/wmctrl -a 'Map' -e '0,1920,1080,1920,1080'
-/usr/bin/wmctrl -a 'Status' -e '0,1,1,1698,843'
+/usr/bin/wmctrl -a 'Status' -e '0,1,1,1936,843'
 /usr/bin/wmctrl -a 'Pluma' -e '0,1,1408,1000,750'
 /usr/bin/wmctrl -a '— Atom' -e '0,965,540,1920,1080'
 /usr/bin/wmctrl -a 'Eclipse' -e '0,965,540,1920,1080'
