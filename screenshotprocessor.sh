dffd495baca700f104d1ca4e0f4fee2eeae63e09
Patrick L Archibald <patrick.archibald@gmail.com>
2019-10-21 08:18:18 -0400

    Make gdrive parent folder ID a variable stored in /etc/environment.

--- screenshotprocessor.sh
+++ screenshotprocessor.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+#
 # Screenshot Processor
 # Called from incron when a file arrives in ~/Pictures.
 # Copies file to /tmp/ss and adds a drop shadow.
@@ -8,18 +9,31 @@
 # incrontab entry sample below:
 # /home/user_joe/Pictures IN_CLOSE_WRITE /home/user_joe/utils/screenshotprocessor.sh $@/$#
 #
+# Also uploads screenshot to Google Drive using gdrive command. You need to set the Google Drive parent
+# folder id in file /etc/environment. The variable name is: GDRIVE_SCREENSHOT_FOLDER_ID
+#
+# Gdrive can be downloaded here: https://github.com/gdrive-org/gdrive
+#
+#
 logger "Start $1 $0"
 export DISPLAY=:0
 filePath="$1"
 file=$(basename "$filePath")
-logger "Basename: $file $0"
 rm -rf /tmp/ss
 mkdir /tmp/ss
-logger "Copy $filePath to /tmp/ss/."
 cp "$filePath" /tmp/ss/.
 newFilePath="/tmp/ss/$file"
-logger "$0 Processing $filePath to $newFilePath"
-convert "$newFilePath" \( -clone 0 -background black -shadow 80x3+0+8 \) -reverse -background none -layers merge +repage "$newFilePath"
-/usr/bin/python /home/htplainf/utils/image2clipboard.py "$newFilePath"
+convert "$newFilePath" \( -clone 0 -background black -shadow 80x3+0+8 \) -reverse -background none -layers merge +repage label:"Screenshot by PLA $(date)" -background white -fill black -font Ubuntu -gravity Center -append "$newFilePath"
+gdriveParentFolder=$(cat /etc/environment | grep GDRIVE_SCREENSHOT_FOLDER_ID | cut -d '=' -f2)
+logger "$0 gdrive parent folder $gdriveParentFolder"
+output=$(/usr/local/bin/gdrive upload --parent "$gdriveParentFolder" "$newFilePath")
+logger "OUTPUT: $output"
+fileId=$(echo "$output" | tr -d '\n' | cut -d ' ' -f 3)
+output=$(/usr/local/bin/gdrive info "$fileId")
+viewUrl=$(echo "$output" | tr '\n' ' ' | cut -d ' ' -f 25)
+echo "$viewUrl" > /tmp/viewUrl.txt
+/usr/bin/xclip -selection clipboard -target text/plain -in /tmp/viewUrl.txt
+sleep 1
+/usr/bin/xclip -selection clipboard -target image/png  -in "$newFilePath"
 /usr/bin/notify-send --icon=/usr/share/icons/Humanity/apps/48/gnome-screenshot.svg "Screenshot ready" "File is $newFilePath"
-logger "$0 Screenshot ready $newFilePath"
+logger "Screenshot ready $newFilePath - $0"
