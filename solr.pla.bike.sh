f20fe60467537b86ac03d9309665bd04fbfe5cfb
Patrick L Archibald (PLA) <Patrick.Archibald@gmail.com>
2020-11-15 03:51:10 -0500

    Add number of seconds in completion message.

--- solr.pla.bike.sh
+++ solr.pla.bike.sh
@@ -2,6 +2,9 @@
 #
 # Crawl https://pla.bike
 #
+logger "$0 Started"
+SECONDS=0
+/opt/solr-7.6.0/bin/post -c bike -type text/xml -out yes -d $'<delete><query>*:*</query></delete>'
 /usr/bin/wget https://pla.bike/sitemap.xml -O /tmp/plabikesitemap.xml
 urls="$(/usr/bin/xpath  -q -e '//loc/text()' /tmp/plabikesitemap.xml)"
 for url in $urls
@@ -12,3 +15,4 @@ do
 		/opt/solr-7.6.0/bin/post -c bike -filestypes html "$url" -delay 1 -recursive 0
 	fi
 done
+logger "$0 Finished in $SECONDS seconds."
